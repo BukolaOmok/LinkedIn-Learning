5352701679a92a6a8c2408d979b2406da1e01c1c
BukolaOmok <157320642+BukolaOmok@users.noreply.github.com>
2024-01-30 09:10:39 +0000

    learn dot notation and bracket notation and when they are used

--- linkedin-html-css-js-training/js-essential-training/03_06/script.js
+++ linkedin-html-css-js-training/js-essential-training/03_06/script.js
@@ -22,3 +22,12 @@ const backpack = {
 };
 
 console.log("The backpack object:", backpack);
+console.log("The pocketNum value:", backpack.pocketNum);  // This is the dot notation but it is limited so we would use the bracket notation
+console.log("Strap Length L:", backpack.strapLength.left);
+
+
+console.log("The pocketNum value:", backpack["pocketNum"]); //This is the bracket notation. It is mnore preferred because it allows you to do the below query. You cannot do it with plain dot notation. This is why it is more preffered. You can use it to parse data that does not parse the rules such as those with hyphen that breaks convention)
+
+var query = "pocketNum";
+console.log("The pocketNum value:", backpack[query]);
+
