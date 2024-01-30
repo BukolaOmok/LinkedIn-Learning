2a6b386f47579084c05b3cfeb80df450b7dcbbaf
BukolaOmok <157320642+BukolaOmok@users.noreply.github.com>
2024-01-30 11:06:00 +0000

    complete js building object practice

--- linkedin-html-css-js-training/js-essential-training/Practice/03_07/script.js
+++ linkedin-html-css-js-training/js-essential-training/Practice/03_07/script.js
@@ -7,3 +7,53 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+// Object
+const handbag = {
+  // Properties of the object
+  name: "Cross Chain Bag",
+  volume: 50,
+  weight: 10,
+  color: "black",
+  pocketNum: 1,
+  strapLength: {
+    left: 30,
+    right: 30,
+  },
+  // Method to toggle lid
+  lidOpen: false, // Lid status for the handbag
+  toggleLid: function (lidStatus) {   
+    this.lidOpen = lidStatus;
+  },
+  // Method to set new strap lengths
+  newStrapLength: function (lengthLeft, lengthRight) {
+    this.strapLength.left = lengthLeft;
+    this.strapLength.right = lengthRight;
+  },
+  // Nested purse object
+  purse: {
+    name: "Small Purse",
+    color: "red",
+    compartments: 2,
+    length: 25, // Length of the purse in centimeters
+    lidOpen: false, // Lid status for the purse
+    toggleLid: function (lidStatus) { 
+      this.lidOpen = lidStatus;
+    },
+  },
+};
+
+
+console.log("The handbag object:", handbag);
+console.log("The purse object:", handbag.purse);
+console.log("The purse color property:", handbag.purse.color);
+
+console.log("The purse object:", handbag["purse"]);
+console.log("The purse color property:", handbag.purse["color"]);
+
+var query = "purse";
+console.log("The purse object", handbag[query])
+
+var query = "color"
+console.log("The purse color property", handbag.purse[query])
+
