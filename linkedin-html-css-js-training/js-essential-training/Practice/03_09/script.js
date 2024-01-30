0213a35390e7fee77b752a82dbb481f397b1e991
BukolaOmok <157320642+BukolaOmok@users.noreply.github.com>
2024-01-30 11:59:34 +0000

    change the length of strap and toggle lid (object methods)

--- linkedin-html-css-js-training/js-essential-training/Practice/03_09/script.js
+++ linkedin-html-css-js-training/js-essential-training/Practice/03_09/script.js
@@ -7,7 +7,10 @@
  * - Test the method by sending new values and checking the properties in the console.
  */
 
+
+//Object
 const backpack = {
+  // Properties of the object
   name: "Everyday Backpack",
   volume: 30,
   color: "grey",
@@ -16,11 +19,28 @@ const backpack = {
     left: 26,
     right: 26,
   },
+
+  // Method to toggle lid
+  lidOpen: true, // Lid status for the backpack
   toggleLid: function (lidStatus) {
     this.lidOpen = lidStatus;
   },
+  // I changed the status of the above in the developer tool on chrome to "false"
+
+  // Method to set new strap lengths
   newStrapLength: function (lengthLeft, lengthRight) {
     this.strapLength.left = lengthLeft;
     this.strapLength.right = lengthRight;
   },
 };
+
+console.log("The backpack object:", backpack)
+
+console.log("left before:", backpack.strapLength.left)
+console.log("right before:", backpack.strapLength.right);
+backpack.newStrapLength(18, 19)
+console.log("left after:", backpack.strapLength.left);
+console.log("right after:", backpack.strapLength.right);
+
+//I changed the length of the strap (left and right) via console. All checks good
+
